f48da08037ef8b6ca155a79fed4d2b992b119dd8
Istiak Morsalin <anik.cse@gmail.com>
2020-05-01 17:33:02 +0600

    functions with multiple return values

--- SwiftPlayGrounds/MyPlayground.playground/Contents.swift
+++ SwiftPlayGrounds/MyPlayground.playground/Contents.swift
@@ -417,3 +417,59 @@ if let removedValue = airports.removeValue(forKey: "DUB") {
 } else {
     print("The airports dictionary does not contain a value for DUB.")
 }
+
+// set
+
+var favoriteGenres: Set = ["Rock", "Classical", "Hip hop"]
+
+print("I have \(favoriteGenres.count) favorite music genres.")
+// Prints "I have 3 favorite music genres."
+
+if favoriteGenres.isEmpty {
+    print("As far as music goes, I'm not picky.")
+} else {
+    print("I have particular music preferences.")
+}
+// Prints "I have particular music preferences."
+
+favoriteGenres.insert("Jazz")
+// favoriteGenres now contains 4 items
+
+
+if let removedGenre = favoriteGenres.remove("Rock") {
+    print("\(removedGenre)? I'm over it.")
+} else {
+    print("I never much cared for that.")
+}
+// Prints "Rock? I'm over it."
+
+
+if favoriteGenres.contains("Funk") {
+    print("I get up on the good foot.")
+} else {
+    print("It's too funky in here.")
+}
+// Prints "It's too funky in here."
+
+for genre in favoriteGenres.sorted() {
+    print("\(genre)")
+}
+// Classical
+// Hip hop
+// Jazz
+
+//functions with multiple return values
+
+func minMax(array: [Int]) -> (min: Int, max: Int) {
+    var currentMin = array[0]
+    var currentMax = array[0]
+    
+    for value in array[1 ..< array.count] {
+        if value < currentMin {
+            currentMin = value
+        } else if value > currentMax {
+            currentMax = value
+        }
+    }
+    return (currentMin, currentMax)
+}
