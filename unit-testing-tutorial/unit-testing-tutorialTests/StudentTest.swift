26a413a0c5cb1e773c51143af03aed9c1c7f6b20
Istiak Morsalin <anik.cse@gmail.com>
2020-05-06 22:49:49 +0600

    started writing test

---
+++ unit-testing-tutorial/unit-testing-tutorialTests/StudentTest.swift
@@ -0,0 +1,24 @@
+//
+//  StudentTest.swift
+//  unit-testing-tutorialTests
+//
+//  Created by ISTIAK on 6/5/20.
+//  Copyright © 2020 istiak. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+@testable import unit_testing_tutorial
+
+class StudentTest: XCTestCase {
+   
+    func testValidFirstName() {
+        
+    }
+    
+    func testValidLastName () {
+        
+    }
+    
+}
